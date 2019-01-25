58b833c11da539707f8aaf435fe620ed6c2d0f39
larryg727 <larrygonzales727@gmail.com>
2019-01-24 21:19:53 -0600

    Added react router to front end client

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
-import React, { Component } from "react";
-import Index from "./components/index";
+import React, { Component } from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import history from './history';
+import Index from './components/index';
+import NotFound from './components/NotFound';
 
 class App extends Component {
     render() {
-        return <Index />;
+        return (
+            <Router history={history}>
+                <Switch>
+                    <Route exact path="/" component={Index} />
+
+                    <Route component={NotFound} />
+                </Switch>
+            </Router>
+        );
     }
 }
 
